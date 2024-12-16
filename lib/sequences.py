57a94e000f57746b21ad137397606591948b5284
MMbats <petermbatia657@gmail.com>
2024-12-16 18:47:02 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    
+    
+    if length <= 0:
+        print ("[]")
+        return []
+    
+    fibonacci = [0, 1]
+    
+    
+    while len(fibonacci) < length:
+        next_value = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_value)
+    
+    
+    fibonacci = fibonacci[:length]
+    
+
+    print(fibonacci)
+    return fibonacci
+
